8814f7af20b147e964de4d341765dcb6d9137e58
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-02 09:01:58 +0530

    added user models

---
+++ src/models/user.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+const userschema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isVarified: {
+        type: Boolean,
+        default: false
+    },
+    otp: {
+        type: String
+    }
+
+},
+    { timestamps: true }
+)
+
+const User = mongoose.model('User', userschema)
+export default User
