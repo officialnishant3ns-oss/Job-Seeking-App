5428d87dee396a164dcb6bd650f2c8396b32a5b8
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-02 09:27:12 +0530

    added  jobsecure models

---
+++ src/models/Jobgiver.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+const JobsGiverschema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    companyName: {
+        type: String
+    },
+    Logo: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    location: {
+        type: String
+    }
+
+
+
+},
+    { timestamps: true }
+)
+
+const JobsGivers = mongoose.model('JobsGiver', JobsGiverschema)
+export default JobsGivers
