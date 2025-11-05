ec49c3ffa802c4c45cd0432459b1b676c36a5c0d
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-05 18:37:08 +0530

    added uploadLogo section there

--- src/controllers/jobGiver.controller.js
+++ src/controllers/jobGiver.controller.js
@@ -1,20 +1,22 @@
 import JobsGivers from '../models/Jobgiver.models.js'
-
+import uploadoncloudinary from '../utils/cloudinary.js'
 
 const JobGiverProfile = async (req, res) => {
     try {
         const { companyName, description, location } = req.body
         if (!companyName || !description || !location) {
-            return res.status(400).json({ message: "something is missing" })
+            return res.status(400).json({ message: "All fields (companyName, description, location) are required" })
         }
-        const userId = req.user.id
-
+        const userId = req.user?._id || req.user?.id
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized user" });
+        }
         const company = await JobsGivers.findOneAndUpdate(
             { userId },
             { companyName, description, location },
             { new: true, upsert: true }
         )
-           res.status(200).json({ message: "Company profile updated", company })
+        res.status(200).json({ message: "Company profile updated", company })
 
 
     } catch (error) {
@@ -23,6 +25,45 @@ const JobGiverProfile = async (req, res) => {
     }
 }
 
+const uploadLogo = async(req,res)=>{
+    try {
+   const userId = req.user?._id || req.user?.id;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized user" });
+    }
+    const LogoPath = req.files?.Logo[0]?.path;
+    if (!LogoPath) {
+      return res.status(400).json({ message: "LogoPath  is required.. " })
 
+    }
+    console.log(LogoPath)
+    const Logo = await uploadoncloudinary(LogoPath)
 
-export { JobGiverProfile }
+    if (!Logo?.secure_url) {
+      return res.status(400).json({ message: "Failed to upload Logo. " })
+    }
+
+      const LogoDetails = await JobsGivers.findOneAndUpdate(
+      { userId },
+      { Logo: Logo.secure_url },
+      { new: true, upsert: true }
+    );
+
+    res.status(200).json({ message: "Logo uploaded", LogoDetails });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+const getMyCompanyProfile = async (req,res) => {
+    try {
+    const userId = req.user.id;
+    const company = await JobsGivers.findOne({ userId });
+    if (!company) return res.status(404).json({ message: "Profile not found" });
+    res.status(200).json(company);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
+}
+
+export { JobGiverProfile,uploadLogo,getMyCompanyProfile }
--- src/routes/JobGiver.profile.routes.js
+++ src/routes/JobGiver.profile.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js"
-import {JobGiverProfile} from '../controllers/jobGiver.controller.js'
+import upload from "../middlewares/multer.controller.js";
+import {JobGiverProfile,uploadLogo,getMyCompanyProfile} from '../controllers/jobGiver.controller.js'
 
 const router = Router()
 
 
 //profile management section
 router.post("/profilemanage",verifyJWT, JobGiverProfile)
-
+router.post('/uploadlogo',verifyJWT,upload.fields([
+        { name: "Logo", maxCount: 1 }
+    ]),
+    uploadLogo
+)
 
 export default router;
